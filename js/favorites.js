5e552bd3edf99acef9303c40a25b6b5bd158b957
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-22 01:09:41 +0600

    Implement functions to manage favorite movies

--- js/favorites.js
+++ js/favorites.js
@@ -1,1 +1,14 @@
-console.log("Favorites ready");
+function getFav(){
+return JSON.parse(localStorage.getItem("fav")) || [];
+}
+
+function addFav(id){
+const m = movies.find(x=>x.id===id);
+let fav = getFav();
+
+if(!fav.find(x=>x.id===id)){
+fav.push(m);
+}
+
+localStorage.setItem("fav",JSON.stringify(fav));
+}
