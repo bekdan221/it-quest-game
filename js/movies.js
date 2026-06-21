b9dbf18289e3e15df435a5aa06291e6b00f81e53
bekdan221 <bekdantemirbekov44@gmail.com>
2026-06-21 22:40:09 +0600

    Update movies.js

--- js/movies.js
+++ js/movies.js
@@ -1,1 +1,18 @@
+let movies = [];
 
+async function loadMovies() {
+    const res = await fetch("data/movies.json");
+    movies = await res.json();
+    renderMovies(movies);
+}
+
+function renderMovies(list) {
+    const container = document.getElementById("movies");
+
+    container.innerHTML = list.map(m =>
+        `<div class="movie">
+            <h3>${m.title}</h3>
+            <p>⭐ ${m.rating}</p>
+        </div>`
+    ).join("");
+}
